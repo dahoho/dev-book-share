51db69313476a40a218f35bc1b8357935415e6f9
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-03-22 19:33:58 +0900

    refactor: uncomment middleware code for clarity and future use

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-// import type { NextRequest } from "next/server";
-// import { auth } from "./auth";
+import type { NextRequest } from "next/server";
+import { auth } from "./auth";
 
-// export default auth(async (req: NextRequest) => {
-//   console.log("req", req);
-// });
+export default auth(async (req: NextRequest) => {
+  console.log("req", req);
+});
 
-// // ミドルウェアを適用するページを指定
-// export const config = {
-//   matcher: [],
-// };
+// ミドルウェアを適用するページを指定
+export const config = {
+  matcher: [],
+};
