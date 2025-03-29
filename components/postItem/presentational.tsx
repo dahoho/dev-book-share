2ac1e179eff50b33f57515f55809140a611506c1
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-03-29 13:40:53 +0900

    fix: refactor PostItemPresentational layout for improved readability and structure

--- components/postItem/presentational.tsx
+++ components/postItem/presentational.tsx
@@ -37,49 +37,54 @@ export const PostItemPresentational = ({
           <PostOperation postId={post.id} />
         </div>
       )}
-      <Link className="block h-full p-4" href={`/post/${post.id}`}>
-        <div className="flex items-center gap-2 justify-between">
-          <div className="flex items-center gap-2">
+      <Link
+        className="flex flex-col justify-between h-full p-4"
+        href={`/post/${post.id}`}
+      >
+        <div>
+          <div className="flex items-center gap-2 justify-between">
             <div className="flex items-center gap-2">
-              <Avatar>
-                <AvatarImage
-                  className={`w-8 rounded-full`}
-                  src={post.author.image ?? ""}
-                  alt={post.author.name ?? ""}
-                />
-              </Avatar>
-              <div className="flex flex-col gap-0.5">
-                <p className="text-md">{post.author.name}</p>
-                <time
-                  dateTime={dayjs(post.createdAt).format(DATE_FORMAT)}
-                  className="text-xs text-gray-500 block"
-                >
-                  {dayjs(post.createdAt).format(DATE_FORMAT)}
-                </time>
+              <div className="flex items-center gap-2">
+                <Avatar>
+                  <AvatarImage
+                    className={`w-8 rounded-full`}
+                    src={post.author.image ?? ""}
+                    alt={post.author.name ?? ""}
+                  />
+                </Avatar>
+                <div className="flex flex-col gap-0.5">
+                  <p className="text-md">{post.author.name}</p>
+                  <time
+                    dateTime={dayjs(post.createdAt).format(DATE_FORMAT)}
+                    className="text-xs text-gray-500 block"
+                  >
+                    {dayjs(post.createdAt).format(DATE_FORMAT)}
+                  </time>
+                </div>
               </div>
+              {isDashboard && (
+                <Badge
+                  className={`text-xs border bg-transparent ${
+                    post.published
+                      ? "text-green-600 border-green-600"
+                      : "text-red-600 border-red-600"
+                  }`}
+                >
+                  {post.published ? "公開中" : "下書き"}
+                </Badge>
+              )}
             </div>
-            {isDashboard && (
-              <Badge
-                className={`text-xs border bg-transparent ${
-                  post.published
-                    ? "text-green-600 border-green-600"
-                    : "text-red-600 border-red-600"
-                }`}
-              >
-                {post.published ? "公開中" : "下書き"}
-              </Badge>
-            )}
           </div>
-        </div>
 
-        <p className="text-xl font-bold mt-4">{post.title}</p>
-        {post.tags.length > 0 && (
-          <ul className="flex items-center flex-wrap gap-2 text-xs mt-4">
-            {post.tags.map((tag) => (
-              <li key={tag}># {tag}</li>
-            ))}
-          </ul>
-        )}
+          <p className="text-xl font-bold mt-4">{post.title}</p>
+          {post.tags.length > 0 && (
+            <ul className="flex items-center flex-wrap gap-2 text-xs mt-4">
+              {post.tags.map((tag) => (
+                <li key={tag}># {tag}</li>
+              ))}
+            </ul>
+          )}
+        </div>
 
         <div className="flex items-center gap-2 mt-6">
           <p className="text-sm flex items-center gap-1">
